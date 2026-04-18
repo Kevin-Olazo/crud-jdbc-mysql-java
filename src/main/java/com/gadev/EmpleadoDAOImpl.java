18e7bb2c70d6af4e610b017a1484dd41231d5910
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-04-17 19:44:50 -0500

    feat: listarTodos method to get all Empleados

--- src/main/java/com/gadev/EmpleadoDAOImpl.java
+++ src/main/java/com/gadev/EmpleadoDAOImpl.java
@@ -3,7 +3,9 @@ package com.gadev;
 import com.gadev.model.Empleado;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,16 @@ public class EmpleadoDAOImpl implements Repositorio<Empleado>{
         List<Empleado> empleados = new ArrayList<>();
         String sql = "SELECT * FROM empleados";
 
-
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()){
+                Empleado emp = mapearEmpleado(rs);
+                empleados.add(emp);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
         return empleados;
     }
 
@@ -35,5 +46,16 @@ public class EmpleadoDAOImpl implements Repositorio<Empleado>{
     @Override
     public void eliminar(int id) {
 
+    }
+
+    // Metodo auxiliar
+    private Empleado mapearEmpleado(ResultSet rs) throws SQLException{
+        Empleado e = new Empleado();
+        e.setId(rs.getInt("id"));
+        e.setNombre(rs.getString("nombre"));
+        e.setPuesto(rs.getString("puesto"));
+        e.setSalario(rs.getDouble("salario"));
+        e.setFechaContratacion(rs.getDate("fecha_contratacion").toLocalDate());
+        return e;
     }
 }
